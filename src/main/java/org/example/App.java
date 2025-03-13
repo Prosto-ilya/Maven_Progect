c436dc191e58b134f4761ec410cad1c1d1e83791
Ilya <ilya.alefirenko2004@gmail.com>
2025-03-13 22:20:38 +0300

    Create Maven Project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public static String getMessage(){
+        return "Hello Word";
+    }
+
+    public static void main(String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
